373f111c351bfe1fa5ab47a713d7029137d22031
Samogonn <solovevym@gmail.com>
2024-02-19 15:14:34 +0300

    menu apis added

--- app/main.py
+++ app/main.py
@@ -1,3 +1,33 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from app import schemas, models
+from app.database import sync_sesion, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency
+def get_session():
+    db = sync_sesion()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/menu/{id}")
+def get_menu(id: int, session: Session = Depends(get_session)):
+    db_menu = session.query(models.Menu).filter(models.Menu.id == id).first()
+    return db_menu
+
+
+@app.post("/menu")
+def add_menu(menu: schemas.MenuCreate, session: Session = Depends(get_session)):
+    db_menu = models.Menu(name=menu.name, description=menu.description)
+    session.add(db_menu)
+    session.commit()
+    session.refresh(db_menu)
+    return db_menu
