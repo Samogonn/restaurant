cc9658954dc13281ecbc6c6fa39d686e4a85fa17
Samogonn <solovevym@gmail.com>
2024-02-20 09:33:05 +0300

    not_found errors added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app import schemas, models, crud
@@ -22,6 +22,8 @@ def get_session():
 @app.get("/menu/{id}")
 def get_menu(id: int, session: Session = Depends(get_session)):
     db_menu = crud.SqlAlchemyCRUD(session).get(id)
+    if db_menu is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return db_menu
 
 
@@ -38,12 +40,17 @@ def add_menu(menu: Annotated[schemas.MenuCreate, Depends()], session: Session = 
 
 @app.patch("/menu/{id}")
 def update_menu(id: int, menu: schemas.MenuUpdate, session: Session = Depends(get_session)):
-    db_menu = crud.SqlAlchemyCRUD(session).update(id, menu)
+    db_menu = crud.SqlAlchemyCRUD(session).get(id)
+    if db_menu is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    crud.SqlAlchemyCRUD(session).update(id, menu)
     return db_menu
 
 
 @app.delete("/menu/{id}")
 def remove_menu(id: int, session: Session = Depends(get_session)):
     db_menu = crud.SqlAlchemyCRUD(session).get(id)
+    if db_menu is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     db_menu = crud.SqlAlchemyCRUD(session).remove(db_menu)
     return db_menu
